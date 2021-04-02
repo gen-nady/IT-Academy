5a210f1760ec5d9d463d7c8830ab2d0b4f14f38e
Gennadiy Shushkevich <gennadiy131@live.com>
2021-04-02 17:56:31 +0300

    Сделал 8 задание. Полнотсью убрал анимации бэкграунда. Сделал все движения заднего плана кодом

---
+++ Zadanie8/Assets/Scripts/Background.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    public GameObject[] background;
+    float speed = 2f;
+    bool isSwitch=false;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            isSwitch = !isSwitch;
+
+        for (int i = 0; i < 2; i++)
+        {
+            if (!isSwitch)
+            {
+                if (background[i].transform.position.x > -6f)
+                    background[i].transform.Translate(Vector2.left * Time.deltaTime * speed);
+                else
+                    background[i].transform.position = new Vector2(6f, background[i].transform.position.y);
+            }
+            else if (isSwitch)
+            {
+                if (background[i].transform.position.x < 6f)
+                    background[i].transform.Translate(Vector2.right * Time.deltaTime * speed);
+                else
+                    background[i].transform.position = new Vector2(-6f, background[i].transform.position.y);
+            }
+
+        }
+        for (int i = 2; i < background.Length; i++)
+        {
+            if (!isSwitch)
+            {
+                if (background[i].transform.position.x > -6f)
+                    background[i].transform.Translate(Vector2.left * Time.deltaTime * speed*speed);
+                else
+                    background[i].transform.position = new Vector2(6f, background[i].transform.position.y);
+            }
+            else if (isSwitch)
+            {
+                if (background[i].transform.position.x < 6f)
+                    background[i].transform.Translate(Vector2.right * Time.deltaTime * speed* speed);
+                else
+                    background[i].transform.position = new Vector2(-6f, background[i].transform.position.y);
+            }
+
+        }
+    }
+}
