c3494ace69423f430742d85ed271ee4f161bb899
Gennadiy Shushkevich <gennadiy13122@gmail.com>
2021-04-24 13:13:24 +0300

    Реализовал задание 2.1

--- Zadanie 2.1/Assets/Scripts/Box.cs
+++ Zadanie 2.1/Assets/Scripts/Box.cs
@@ -1,16 +1,15 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 [RequireComponent(typeof(MeshFilter))]
 [RequireComponent(typeof(MeshRenderer))]
-//[RequireComponent(typeof(MeshCollider))]
 public class Box : MonoBehaviour
 {
-    Mesh mesh, meshCut, meshNew;
-    // Start is called before the first frame update
+    Mesh mesh, meshNew;
+    GameObject newObj;
+
     void Start()
     {
+        //создание первого обьекта и делаем его квадратным
         mesh = new Mesh();
         GetComponent<MeshFilter>().mesh = mesh;
         mesh.vertices = GenetateVertices();
@@ -18,59 +17,73 @@ public class Box : MonoBehaviour
         mesh.RecalculateNormals();
         MeshCollider meshc = gameObject.AddComponent(typeof(MeshCollider)) as MeshCollider;
         meshc.sharedMesh = mesh;
-        Instantiate(gameObject, Vector3.zero, Quaternion.identity);
-        //пытаюсь создать 2 новыъ объекта треугольника
-        meshCut = new Mesh();
-        GetComponent<MeshFilter>().mesh = meshCut;
-        meshNew = new Mesh();
-        GetComponent<MeshFilter>().mesh = meshNew;
-        meshNew.vertices = GenetateVerticesOne();
-        meshNew.triangles = GenerateOne();
-        meshCut.vertices = GenetateVerticesTwo();
-        meshCut.triangles = GenerateTwo();
-    
+    }
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            //создаем второй обьект треугольник
+            newObj = new GameObject("NewBox");
+            meshNew = new Mesh();
+            newObj.AddComponent<MeshFilter>();
+            newObj.AddComponent<MeshRenderer>();
+            newObj.GetComponent<MeshFilter>().mesh = meshNew;
+            meshNew.vertices = GenetateVerticesTwo();
+            meshNew.triangles = GenerateTriangles();
+            meshNew.RecalculateNormals();
+            MeshCollider meshc = newObj.AddComponent<MeshCollider>();
+            newObj.transform.GetComponent<MeshCollider>().convex = true;
+            newObj.AddComponent<Rigidbody>();
+
+            //первый обьект преврщаем в треугольник
+            mesh.vertices = GenetateVerticesOne();
+            mesh.triangles = GenerateTriangles();
+        }
     }
     Vector3[] GenetateVertices()
     {
         return new Vector3[]
         {
                 new Vector3(0f, 0f, 0f),
-                new Vector3(0f, 0f, 1f),
                 new Vector3(1f, 0f, 0f),
-                new Vector3(1f, 0f, 1f)
-
+                new Vector3(1f, 0f, 1f),
+                new Vector3(0f, 0f, 1f),
+                new Vector3(0f, 1f, 0f),
+                new Vector3(1f, 1f, 0f),
+                new Vector3(1f, 1f, 1f),
+                new Vector3(0f, 1f, 1f)
         };
     }
     Vector3[] GenetateVerticesOne()
     {
         return new Vector3[]
         {
-                new Vector3(0f, 0f, 0f),
-                new Vector3(0f, 0f, 1f),
-                new Vector3(1f, 0f, 0f)
-
+               new Vector3(0f, 0f, 0f),
+                new Vector3(1f, 0f, 0f),
+                new Vector3(1f, 0f, 0.5f),
+                new Vector3(0f, 0f, 0.5f),
+                new Vector3(0f, 1f, 0f),
+                new Vector3(1f, 1f, 0f),
+                new Vector3(1f, 1f, 0.5f),
+                new Vector3(0f, 1f, 0.5f)
         };
     }
     Vector3[] GenetateVerticesTwo()
     {
         return new Vector3[]
         {
+                 new Vector3(0f, 0f, 0.5f),
+                new Vector3(1f, 0f, 0.5f),
+                new Vector3(1f, 0f, 1f),
+                new Vector3(0f, 0f, 1f),
+                new Vector3(0f, 1f, 0.5f),
+                new Vector3(1f, 1f, 0.5f),
                 new Vector3(1f, 1f, 1f),
-                new Vector3(1f, 1f, 2f),
-                new Vector3(2f, 1f, 1f)
-
+                new Vector3(0f, 1f, 1f)
         };
     }
     int[] GenerateTriangles()
     {
-        return new int[] { 0, 1, 2 , 1,3,2};
-    }
-    int[] GenerateOne()
-    {
-        return new int[] { 0, 1, 2};
-    }
-    int[] GenerateTwo()
-    {
-        return new int[] { 0, 1, 2 };
+        return new int[] { 0, 1, 3, 1, 2, 3, 0, 4, 5, 0, 5, 1, 7, 6, 5, 7, 5, 4, 3, 2, 6, 3, 6, 7, 7, 0, 3, 7, 4, 0, 6, 2, 1, 6, 1, 5 };
     }
 }
