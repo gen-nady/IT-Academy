05e16ec273b44f27d9d8583673549171f6cf3a6a
Gennadiy Shushkevich <gennadiy131@live.com>
2021-04-27 20:31:54 +0300

    Загрузка задания 2.3

--- Zadanie4/Assets/Scripts/Boom.cs
+++ Zadanie4/Assets/Scripts/Boom.cs
@@ -6,11 +6,17 @@ public class Boom : MonoBehaviour
 {
     public float radius = 5.0F;
     public float power = 10.0F;
-
+    public ParticleSystem ps;
+    public GameObject go;
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
         {
+            ps.transform.position = transform.position;
+            ps.Play(true);
+            //go.transform.position = transform.position;
+            //go.SetActive(true);
+            Debug.Log("Check");
             Vector3 explosionPos = transform.position;
             Collider[] colliders = Physics.OverlapSphere(explosionPos, radius);
             foreach (Collider hit in colliders)
@@ -18,7 +24,10 @@ public class Boom : MonoBehaviour
                 Rigidbody rb = hit.GetComponent<Rigidbody>();
 
                 if (rb != null)
+                {
                     rb.AddExplosionForce(power, explosionPos, radius, 3.0F);
+                    
+                }
             }
         }
     }
